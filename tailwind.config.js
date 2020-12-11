2fbd528a11d292a2e3b6ae6df643b7200eaab4dc
Soren Thompson <sorenct04@gmail.com>
2020-12-10 19:34:50 -0700

    redo tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,14 +12,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        gray: colors.coolGray,
-        blue: colors.blue,
-        red: colors.red,
-        yellow: colors.amber,
-        green: colors.emerald,
-        indigo: colors.indigo,
-        purple: colors.violet,
-        pink: colors.pink,
+        ...colors
       },
       fontSize: {
         xxs: "0.625rem",
