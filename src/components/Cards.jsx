7c6072992e88dc46a4415eff2634798de40b4652
Soren Thompson <sorenct04@gmail.com>
2020-12-11 12:38:28 -0700

    remove extra div

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -13,21 +13,19 @@ export const Cards = ({ cards }) => {
   return (
     <div className="container pt-6 overflow-hidden">
       <div className="flex flex-wrap -mx-3 xl:-mx-6">
-        <div className="w-full sm:w-1/2 xl:w-1/3 px-3 xl:px-6 py-6">
-          {empty ? (
-            <div className="w-full sm:w-1/2 xl:w-1/3 px-3 xl:px-6 py-6">
-              <Card {...emptyState} />
+        {empty ? (
+          <div className="w-full sm:w-1/2 xl:w-1/3 px-3 xl:px-6 py-6">
+            <Card {...emptyState} />
+          </div>
+        ) : cards.map((card) => {
+          return (              
+            <div 
+              key={`card_${card.slug}`}
+              className="w-full sm:w-1/2 xl:w-1/3 px-3 xl:px-6 py-6">
+              <Card {...card} />
             </div>
-          ) : cards.map((card) => {
-            return (              
-              <div 
-                key={`card_${card.slug}`}
-                className="w-full sm:w-1/2 xl:w-1/3 px-3 xl:px-6 py-6">
-                <Card {...card} />
-              </div>
-            );
-          })}
-        </div>  
+          );
+        })}
       </div>
     </div>
   )
