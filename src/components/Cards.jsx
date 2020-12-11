d498a8628174dc1eb16b4830c351a38fa9549579
Soren Thompson <sorenct04@gmail.com>
2020-12-11 12:27:41 -0700

    fix unique key needed for cards map

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -13,13 +13,13 @@ export const Cards = ({ cards }) => {
   return (
     <div className="container pt-6 overflow-hidden">
       <div className="flex flex-wrap -mx-3 xl:-mx-6">
-        <div
-          className="w-full sm:w-1/2 xl:w-1/3 px-3 xl:px-6 py-6"
-          key={`card_${card.slug}`}>
+        <div className="w-full sm:w-1/2 xl:w-1/3 px-3 xl:px-6 py-6">
           {empty ? (
             <Card {...emptyState} />
           ) : cards.map((card) => {
-            return <Card {...card} />;
+            return (
+              <Card key={`card_${card.slug}`} {...card} />
+            );
           })}
         </div>  
       </div>
